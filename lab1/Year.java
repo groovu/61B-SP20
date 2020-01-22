780a40367d59ed6a962d872ee69b7d08718743fe
Cherish Truong <truong@berkeley.edu>
2020-01-22 13:50:53 -0800

    Completed

--- lab1/Year.java
+++ lab1/Year.java
@@ -1,11 +1,11 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Cherish Truong
  */
 public class Year {
 
     /** Return true iff YEAR is a leap year.  */
     static boolean isLeapYear(int year) {
-        return true;    // TODO: YOUR CODE HERE
+        return ((year % 400 == 0) || ((year % 4 ==0) && (year % 100 != 0)));    // TODO: YOUR CODE HERE
     }
 
     /** Print whether YEAR is a a leap year on System.out. */
